b50579ba7b5a3cc73d5a8f322f8f2e3758d8666c
Aniruddh Laxman Korde <aniruddhkorde9@gmail.com>
2026-01-22 14:29:57 +0530

    refactor: convert CopyOptions to record with builder

--- src/main/java/land/oras/CopyOptions.java
+++ src/main/java/land/oras/CopyOptions.java
@@ -1,30 +1,57 @@
+/*
+ * Copyright The ORAS Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package land.oras;
 
 /**
- * Options for the copy operation.
+ * Options for copy operations.
+ *
+ * @param recursive true to recursively copy artifacts
  */
-public class CopyOptions {
+public record CopyOptions(boolean recursive) {
 
     /**
-     * Whether to copy referrers recursively.
+     * Creates a new builder for CopyOptions.
+     * @return a new builder
      */
-    private boolean recursive;
-
-    // TODO: In the future, we will add 'depth' and 'filter' here!
-
-    /**
-     * Constructor
-     * @param recursive true to copy referrers, false otherwise
-     */
-    public CopyOptions(boolean recursive) {
-        this.recursive = recursive;
+    public static Builder builder() {
+        return new Builder();
     }
 
     /**
-     * Gets the recursive setting.
-     * @return true if recursive
+     * Builder for CopyOptions.
      */
-    public boolean isRecursive() {
-        return recursive;
+    public static class Builder {
+        private boolean recursive;
+
+        /**
+         * Sets whether to recursively copy artifacts.
+         * @param recursive true to recursively copy
+         * @return this builder
+         */
+        public Builder recursive(boolean recursive) {
+            this.recursive = recursive;
+            return this;
+        }
+
+        /**
+         * Builds the CopyOptions.
+         * @return the CopyOptions
+         */
+        public CopyOptions build() {
+            return new CopyOptions(recursive);
+        }
     }
 }
