2535385e44cd6d3b314757186eccf88fa5340137
Valentin Delaye <jonesbusy@users.noreply.github.com>
2026-02-05 05:42:17 +0100

    Update license header for 2026
    Signed-off-by: Valentin Delaye <jonesbusy@users.noreply.github.com>

--- src/test/java/land/oras/LocalPathTest.java
+++ src/test/java/land/oras/LocalPathTest.java
@@ -2,7 +2,7 @@
  * =LICENSE=
  * ORAS Java SDK
  * ===
- * Copyright (C) 2024 - 2025 ORAS
+ * Copyright (C) 2024 - 2026 ORAS
  * ===
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
